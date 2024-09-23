b46a9b4c5f5ac237db891b0d200158992f06aebb
paperdev-code <51487819+paperdev-code@users.noreply.github.com>
2024-09-23 14:33:13 +0200

    init

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "paperdev.github.io";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+      ];
+      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            http-server
+            just
+            nodemon
+            tailwindcss
+          ];
+        };
+        formatter = pkgs.nixpkgs-fmt;
+      };
+    };
+}
---
+++ justfile
@@ -0,0 +1,22 @@
+# build static page, stores in ./docs
+@build:
+  mkdir -p docs
+  @# copying static files
+  cp -r src/index.html docs/
+  @# building tailwind css
+  tailwindcss -i src/style.css -o docs/style.css -m
+  @# done!
+
+# clean ./docs directory
+@clean:
+  @# deleting `docs` directory.
+  rm -rf docs
+
+@serve:
+  just build
+  @# serving http://127.0.0.1:8080
+  NODE_NO_WARNINGS=1 http-server docs -s -a 127.0.0.1 -p 8080
+
+# automatically rebuild on file change
+@watch:
+  nodemon --watch src --watch tailwind.config.js -e css,html --exec just serve
